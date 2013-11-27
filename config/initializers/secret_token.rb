c99ffac8626355a67b03251aaffdc3addbbf7d17
Monirul Islam <sajibnstu@gmail.com>
2013-11-27 12:38:36 +0600

    Uploaded all files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lynda::Application.config.secret_token = '097511bcb81b3a0ad87afd55297efaf0041a0e7cf1e474eb983040506d3ecde299e0333d01f937b1f98caaaae55062612cee6e668d3d7694a982f2cf5019a7ff'
